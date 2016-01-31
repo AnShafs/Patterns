1c2494199bd875517e4926101e455612c966c8c6
anshafs <anhafs@gmail.com>
2016-01-31 15:38:11 +0300

    Realize for one Thread

--- Singleton/src/Singleton.java
+++ Singleton/src/Singleton.java
@@ -2,4 +2,15 @@
  * Created by Shafs on 31.01.2016.
  */
 public class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
 }
